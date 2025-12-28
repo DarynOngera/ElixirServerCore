3943de5e3d005806e569e4a1d50360ed01c5cafc
Ongera <ongeradaryn@gmail.com>
2025-12-28 06:22:14 +0300

    core: expose server for external testing

--- elixir_server_core/lib/elixir_server_core/application.ex
+++ elixir_server_core/lib/elixir_server_core/application.ex
@@ -1,12 +1,18 @@
 defmodule ElixirServerCore.Application do
   use Application
+  require Logger
 
   @impl true
   def start(_type, _args) do
-    children = [
-      Core.Workers.JobQueue
-    ]
+    port = 4000
 
+    children = [
+      {Plug.Cowboy, scheme: :http, plug: Core.HTTP.Router, options: [port: port, ip: {0,0,0,0}]},
+      Core.Workers.JobQueue,
+      Core.Workers.Worker
+    ]
+    Logger.info("Starting server on port #{port}")
+    Logger.info("http://localhost:4000")
     Supervisor.start_link(
       children,
       strategy: :one_for_one,
