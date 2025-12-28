30ae2fb0ca94cae20b2a0b8fdc7ab213d1a433fe
Ongera <ongeradaryn@gmail.com>
2025-12-28 03:59:17 +0300

    deps: add telemtry_metrics

---
+++ elixir_server_core/mix.exs
@@ -0,0 +1,29 @@
+defmodule ElixirServerCore.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :elixir_server_core,
+      version: "0.1.0",
+      elixir: "~> 1.14",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger],
+      mod: {ElixirServerCore.Application, []}
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+       {:telemetry, "~> 1.2"},
+       {:telemetry_metrics, "~> 0.6"}
+    ]
+  end
+end
